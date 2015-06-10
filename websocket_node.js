f6408bd9a30d3cec43ee462142e785d867fde9a1
its2mc <its2uraps@msn.com>
2015-06-10 12:03:56 +0300

    Some basic errors.. some error logs done

--- websocket_node.js
+++ websocket_node.js
@@ -29,7 +29,6 @@ comms.init(handle); //Pass message handling function to sub_socket
 ws.on('connection', function (ws_sock) {
     ws_sock.on('message', function (message) {
   		console.log(message);
-//		logger.logStat(message);
 		if(comms.transmit(message))
 			console.log("Message Sent");
 //			logger.logStat("Message Sent");
