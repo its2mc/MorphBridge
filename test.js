1bf78a7b112cb353339092224151b9e5ba6b5e77
its2mc <its2uraps@msn.com>
2015-06-11 11:49:49 +0300

    Some new additions and changes, finally structure begins to work.. somewhat..

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ var wsPort = 9090;
 var ws = new WebSocket('ws://localhost:'+wsPort );
 
 ws.on('open', function open() {
-  var array = new Float32Array(5);
+	var array = new Float32Array(5);
 
-  for (var i = 0; i < array.length; ++i) {
-    array[i] = i / 2;
-  }
-
-  ws.send(array, { binary: true, mask: true });
+	for (var i = 0; i < array.length; ++i) {
+		array[i] = i / 2;
+	}
+ 	for(var i = 0; i< 10; i++)
+ 		ws.send(array, { binary: true, mask: true });
 });
