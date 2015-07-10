67048313ab50d8d7392a2ee8ba20896902b0ae10
its2mc <its2uraps@msn.com>
2015-07-10 20:21:32 +0300

    Polished it a bit more...
    Hid some of the infrastructure and cleared it to look more professional

--- test.js
+++
@@ -1,9 +0,0 @@
-var WebSocket = require('ws');
-var wsPort = 9090;
-var ws = new WebSocket('ws://localhost:'+ wsPort );
-
-ws.on('open', function open() {
-
- 	for(var i = 0; i< 10; i++)
- 		ws.send("{5,0,'Hey'}");
-});
